d68832633d3b8355c33c7ea1ddb86db433346893
kavishatgit <kavish344@gmail.com>
2024-01-31 01:47:04 +0530

    added a file named as main.tf contains a script of virtual machine

--- main.tf
+++ main.tf
@@ -42,4 +42,24 @@ features   {}
      public_ip_address_id   =   azurerm_public_ip.myvm1publicip.id
    }
  }
+ resource   "azurerm_windows_virtual_machine"   "my-vm"   {
+   name                    =   "myvm1"  
+   location                =   "northeurope"
+   resource_group_name     =   azurerm_resource_group.rg.name
+   network_interface_ids   =   [ azurerm_network_interface.myvm1nic.id ]
+   size                    =   "Standard_B1s"
+   admin_username          =   "adminuser"
+   admin_password          =   "Password123!"
  
+   source_image_reference   {
+     publisher   =   "MicrosoftWindowsServer"
+     offer       =   "WindowsServer"
+     sku         =   "2019-Datacenter"
+     version     =   "latest"
+   }
+ 
+   os_disk   {
+     caching             =   "ReadWrite"
+     storage_account_type   =   "Standard_LRS"
+   }
+ }
