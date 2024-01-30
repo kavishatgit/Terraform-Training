b36eb0503e3e4c83ccb330c6b5545011d122adcd
kavishatgit <kavish344@gmail.com>
2024-01-31 01:32:49 +0530

    added a file named as main.tf contains a script of virtual machine

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider   "azurerm"   {
+ 
+features   {}
+ }
+ 
+ resource   "azurerm_resource_group"   "rg"   {
+   name   =   "my-first-terraform-rg"
+   location   =   "northeurope"
+ }
+ 
+ resource   "azurerm_virtual_network"   "myvnet"   {
+   name   =   "my-vnet"
+   address_space   =   [ "10.0.0.0/16" ]
+   location   =   "northeurope"
+   resource_group_name   =   azurerm_resource_group.rg.name
+ }
+ 
+ resource   "azurerm_subnet"   "frontendsubnet"   {
+   name   =   "frontendSubnet"
+   resource_group_name   =    azurerm_resource_group.rg.name
+   virtual_network_name   =   azurerm_virtual_network.myvnet.name
+   address_prefixes   =   [ "10.0.1.0/24" ]
+ }
+ 
+ resource   "azurerm_public_ip"   "myvm1publicip"   {
+   name   =   "pip1"
+   location   =   "northeurope"
+   resource_group_name   =   azurerm_resource_group.rg.name
+   allocation_method   =   "Dynamic"
+   sku   =   "Basic"
+ }
+ 
+ resource   "azurerm_network_interface"   "myvm1nic"   {
+   name   =   "myvm1-nic"
+   location   =   "northeurope"
+   resource_group_name   =   azurerm_resource_group.rg.name
+ 
+   ip_configuration   {
+     name   =   "ipconfig1"
+     subnet_id   =   azurerm_subnet.frontendsubnet.id
+     private_ip_address_allocation   =   "Dynamic"
+     public_ip_address_id   =   azurerm_public_ip.myvm1publicip.id
+   }
+ }
+ 
